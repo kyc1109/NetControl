d067817af06b75eec5775b5912c67812c43d7a10
kyc1109 <kyc1109@gmail.com>
2019-06-27 09:24:25 +0800

    update
    update for the current status

--- Ethan_C.py
+++ Ethan_C.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
 #coding=utf8
-#sys.setdefaultencoding('utf8')
 
 #主控端, 需求:能遠端執行命令或資料夾
-
 import socket, os, sys, time, datetime, threading         # Import socket module
 from subprocess import Popen   #http://www.bogotobogo.com/python/python_network_programming_server_client_file_transfer.php
 from importlib import reload
 reload(sys)
-#sys.setdefaultencoding('utf8')
-# -*- coding: UTF-8 -*-
-def get_control(UUT_IP, UUT_cmd):
+
+def get_control(UUT_IP, UUT_cmd): #no response
     try:
         #0==================================
-        port = 60001      
+        port = 80808      
         s = socket.socket()             # Create a socket object
         host = socket.gethostname()     # Get local machine name
         #print("H:Controller IP:",socket.gethostbyname(socket.getfqdn()))  # Reserve a port for your service.
@@ -24,93 +21,15 @@ def get_control(UUT_IP, UUT_cmd):
         s.settimeout(1)
         s.connect((UUT_IP, port))    #s.connect((host, port)) 
         s.settimeout(5)
-        #10.110.146.77
-        #10.110.136.52
-        #10.110.136.159
-        #10.110.145.250
-        #10.110.146.9
-        #192.168.0.178
-        #10.110.141.78
-
-        #s.send(b"Hello UUT!") #S0  UUT 掛掉的地方
-
-
         #2==================================
-
-
-
-        #while True :
-        #remote_cmd_str = input("\nplease input the cmd (default is dir): \n") #"please input the cmd (default is dir):", "dir"
         remote_cmd_str = UUT_cmd # str(input("\nplease input the cmd (default is help): \n") or "help")
         remote_cmd = remote_cmd_str.encode('utf-8')
         if not remote_cmd.lower():    #for receive others data
             print("Do nothing")
-            """
-            print("hang here")
-            recv_data = s.recv(1024) #hang on here
-            if not recv_data:
-                print("No data")
-            else:
-                recv_data = s.recv(1024)
-                print ("\nReceive... \n"+ recv_data)      
-            """
-        elif remote_cmd.lower() == "exit" or remote_cmd.lower() == "q":
-            s.send(b"Close by client")
-            s.close()
         else:
             s.send(remote_cmd)   #S1
-            #print('H:Sending... ',repr(remote_cmd_str)) #2/26 remove cmd to U
-
-        time.sleep(1)   #delay
-        """
-        recv_data = s.recv(1024)    #R1, buffer not enough
-        if not recv_data:
-            print("no data")
-            break
-        else:
-            print ("\nReceive... \n"+ recv_data)
-            recv_data = s.recv(1024)  # keep waiting for data coming. Bug at here
-            if not recv_data:
-                s.close()
-                break
-            time.sleep(1)
-        """
-        try:#response from UUT
-            s.setblocking(0)
-            total_data="";data="";begin=time.time();timeout=2
-            UUT_Return = s.recv(1024).decode("utf-8")
-            #print("remote:",UUT_Return) #2/26 remove ack from U
-        except:
-            print("Command error")
-         #str type with decode.
-        """
-        while True: #1
-            if total_data and time.time()-begin>timeout:
-                break
-            elif time.time()-begin>timeout*2:
-                break
-            try:
-                data=s.recv(1024) #1024, Type Error: must be str, not bytes
-                #print("data recv")
-                if data is not None:
-                    total_data=total_data+data.decode("utf-8","ignore")
-                    #print("data ++")
-                    begin=time.time()
-                else: 
-                    time.sleep(0.1)
-            except Exception as e:
-                raise e
-            #print("total_data")
-            print("remote:",total_data) #show txt feedback of UUT, total_data.decode('gb2312', 'ignore').encode('utf-8')
-            
-            #wf = open("total_data.txt",'w')  #write file TestItem4_1.txt ~ TestItem4_11.txt
-            #for w in total_data:   #var TestItem4_1~TestItem4_11
-            #    wf.write(w.decode('gb2312', 'ignore').encode('utf-8'))                       #write TestItem4_1 to TestItem4_1.txt, TestItem4_2 to TestItem4_2.txt ...
-            #wf.close()
-        """
-        return UUT_Return 
         s.close()
-            #break
+
     except socket.timeout as msg:
         #print("Socket Error: %s" % msg)
         print("%s is not found" % UUT_IP) 
@@ -123,34 +42,65 @@ def get_control(UUT_IP, UUT_cmd):
     except TypeError as msg:
         print("Type Error: %s" % msg)   
     finally: 
-        #print("Sever: " +s.recv(1024))
         s.close() 
-        
-        #print('finally connection closed')
-        #print("press any key for input next command")
-        #os.system("pause")
-#p=Popen('received_file.bat')
-#stdout,stderr =p.communicate()
 
+def get_response(UUT_IP, UUT_cmd): #get control and has response
+    global UUT_IPScans
+    UUT_IPScans=[]
+    try:
+        port = 80808      
+        s = socket.socket()             # Create a socket object
+        host = socket.gethostname()     # Get local machine name
+        UUT_Return=""
+        s.settimeout(2)
+        s.connect((UUT_IP, port))    #s.connect((host, port)) 
+        s.settimeout(5)
+        remote_cmd_str = UUT_cmd # str(input("\nplease input the cmd (default is help): \n") or "help")
+        remote_cmd = remote_cmd_str.encode('utf-8')
+        if not remote_cmd.lower():    #for receive others data
+            print("Do nothing")
+        else:
+            s.send(remote_cmd)   #S1
+
+        recv_data = s.recv(1024)    #R1, buffer not enough
+        if not recv_data:
+            print("no data")
+            #break
+        else: #get (host, IP, MAC) from UUT
+            getStr=recv_data.decode("utf-8")
+            UUTinfo=getStr.split(",")
+            UUT_IPScans.append(UUTinfo[2])
+            print ("\nReceive... \n"+ getStr)
+            recv_data = s.recv(1024)  # keep waiting for data coming. Bug at here
+            if not recv_data:
+                s.close()
+                #break
+            time.sleep(1)
+        s.close()
+    except socket.timeout as msg:
+        pass
+        #print("%s is not found" % UUT_IP) 
+    except socket.error as msg:
+        print("Socket Error: %s" % msg)
+        print("No this UUT: %s" % UUT_IP) 
+    except IOError as e:
+        print("I/O error({0}): {1}".format(e.errno, e.strerror))
+    except TypeError as msg:
+        print("Type Error: %s" % msg)   
+    finally: 
+        s.close() 
 
 def main_menu():
     global UUT_IPs
-    #10.110.146.77
-    #10.110.136.52
+    global UUT_IPScans
 
     list=["test", "test2", "test3"]
     print("""
      -----------------------------------------------------------------
-     |               Stress Test Menu      DQA IoT/A73               |
+     |                    Server/Client Test               |
      -----------------------------------------------------------------
-         %s                     %s
-         %s                           %s
-         %s                           %s
-         %s                           %s
-         %s                %s
-        %s           %s
-        %s            %s
-        %s
+         %s                    %s
+
      -----------------------------------------------------------------
      |  Q. Exit   {0}                                |
      |  {1}                             |
@@ -158,27 +108,32 @@ def main_menu():
 
         """.format((datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")),
             "Host/Controller IP:"+socket.gethostbyname(socket.getfqdn())) 
-        %("A. Scan UUT", "2. Send cmd", 
-            "3. S3", "4. S4",
-            "5. S5","6. MS", 
-            "7. WB", "8. HyS5",
-            "9. Get UUT Info","10. Send run file",
-            "11. Sync Up Date/Time", "12. Update_Stress",
-            "13. Load IP txt file", "14. Show IP list",
-            "15. UUT reboot"))
-    UUT_cmd = str(input("\nplease input the cmd (default is 14): \n") or "14") 
+        %("A. Scan UUT", "*2. Kill all of task"))
 
+    UUT_cmd = str(input("\nplease input the cmd (default is 14): \n") or "14") 
     if not UUT_cmd.lower():
         print("Do nothing")
+    elif (UUT_cmd.lower() == "a"):
+        UUT_cmd="__scan_uut__"
+        UUT_IPs=[] #Clean all IP in UUT_IPs 
+        try:
+            ipRange = str(input("\nplease input the IP range (ex: 192.168.0.*), I will scan IP 192.168.0.1-254 (default is \"192.168.0.*\"): \n") or "192.168.0.*")
+            ipRangeBody = ipRange.replace("*","")
+            for i in range(1,255,1):
+                UUT_IPs.append(str(ipRangeBody+str(i)))
+            #print(UUT_IPs)
+            #print("Total IP:",UUT_IPs.__len__())
+        except:
+            print("Error, only support C class")
+            UUT_IPs=""
+    elif (UUT_cmd.lower() == "2"):
+        UUT_cmd="taskkill /F /IM bit.exe /IM Heaven.exe /IM browser_x86.exe /IM FurMark.exe /IM java* /IM 3DMark* /IM ThermalAnalysisToolCmd* /IM Prime95* /IM STPM* /IM ffxv.exe /IM wmplayer.exe /IM nvp* /T"
     elif (UUT_cmd.lower() == "11"):
         DateNow=datetime.datetime.now().strftime("%m-%d-%y")
         TimeNow=datetime.datetime.now().strftime("%H:%M:%S")
         #date mm-dd-yy |time hh:mm:ss
         UUT_cmd="date "+DateNow+"|time "+TimeNow
         print("Now "+DateNow+" "+TimeNow)
-    elif (UUT_cmd.lower() == "12"):
-        UUT_cmd="start update_stress.exe"
-
     elif (UUT_cmd.lower() == "13"):
         UUT_cmd=""
         try:
@@ -193,54 +148,31 @@ def main_menu():
             UUT_IPs=""
     elif (UUT_cmd.lower() == "14"):
         UUT_cmd=""
+        print("IP lists:")
         for IP in UUT_IPs:
             print(IP)
+        print("Total IP:",UUT_IPs.__len__())
     elif (UUT_cmd.lower() == "15"):
         UUT_cmd="shutdown -r -f -t 0"
-
+    elif (UUT_cmd.lower() == "22"):
+        UUT_cmd=""
+        IP_export=""
+        for IP in UUT_IPs:
+            IP_export=IP_export+IP+"\n"
+            print(IP)
+        ip = open("IP_export.txt","w",encoding="utf-8")
+        ip.write(IP_export)
+            #IP_export.write(ip)
+        ip.close()
+        print("Export IP to IP_export.txt")
+    elif (UUT_cmd.lower() == "help"):
+        UUT_cmd=""
+        help()
     elif (UUT_cmd.lower() == "exit" or UUT_cmd.lower() == "q" or UUT_cmd.lower() == "qq"):
+        print("Goodbye")
         sys.exit(0)
-
-
-    """
-    #Excute cmd by multi threading method 2, No good
-    if len(sys.argv)>1:
-        #single UUT
-        UUT_IP=sys.argv[1]
-        try:
-            UUT_Return = str(get_control(UUT_IP,UUT_cmd)) #ip, cmd
-            if UUT_Return=="U:OK, just do it.":
-                print("C:OK")
-            else:
-                print("C:UUT_Return:",UUT_Return)
-        except:
-            print(UUT_IP,"connection fail.")
-    else:
-        if UUT_IPs=="": #
-            print("No IP input")
-        elif UUT_cmd=="":
-            print("No command")
-        else:
-            #for multi UUT
-            from multiprocessing.pool import ThreadPool
-            pool = ThreadPool(processes=4)
-            for UUT_IP in UUT_IPs:
-                #while True:
-                #print(UUT_IP)
-                try:
-                    async_result = pool.apply_async(get_control, (UUT_IP,UUT_cmd)) # tuple of args for foo
-                    UUT_Return = async_result.get()
-                    if UUT_Return=="U:OK, just do it.":
-                        print("C:OK")
-                    else:
-                        print("C:UUT_Return:",UUT_Return)
-                except:
-                    print(UUT_IP,"connection fail.")
-
-    """
-
     
-    #Excute cmd by multi threading method 1, use this
+    #single thread method 1, use this
     if len(sys.argv)>1:
         #single UUT
         UUT_IP=sys.argv[1]
@@ -252,16 +184,31 @@ def main_menu():
                 print("C:UUT_Return:",UUT_Return)
         except:
             print(UUT_IP,"connection fail.")
-    else:
+    else: #multi thread
         if UUT_IPs=="": #
             print("No IP input")
         elif UUT_cmd=="":
             pass
             #print("No command")
+        elif UUT_cmd=="__scan_uut__": #for scan uut
+            threads=[]
+            for UUT_IP in UUT_IPs:
+                try:
+                    threads.append(threading.Thread(target=get_response,args=(UUT_IP,UUT_cmd)))
+                    try:
+                        threads[UUT_IPs.index(UUT_IP)].start()
+                    except timeout:
+                        print("thread connection fail.")
+                except:
+                    print(UUT_IP,"connection fail.")
+            for UUT_IP in UUT_IPs:
+                threads[UUT_IPs.index(UUT_IP)].join()
+            UUT_IPs=UUT_IPScans # replace IP list by scan IP
         else:
             #for multi UUT
             threads=[]
-            for UUT_IP in UUT_IPs:
+            #command to all of UUT
+            for UUT_IP in UUT_IPs: 
                 #while True:
                 #print(UUT_IP)
                 try:
@@ -281,50 +228,15 @@ def main_menu():
             for UUT_IP in UUT_IPs:
                 threads[UUT_IPs.index(UUT_IP)].join()
 
-    """
-    #Excute cmd by single thread
-    if len(sys.argv)>1:
-        #single UUT
-        UUT_IP=sys.argv[1]
-        try:
-            UUT_Return = str(get_control(UUT_IP,UUT_cmd)) #ip, cmd
-            if UUT_Return=="U:OK, just do it.":
-                print("C:OK")
-            else:
-                print("C:UUT_Return:",UUT_Return)
-        except:
-            print(UUT_IP,"connection fail.")
+def help():
+    print("""
+This is help...
+        """)
 
-    else:
-        if UUT_IPs=="": #
-            print("No IP input")
-        elif UUT_cmd=="":
-            print("No command")
-        else:
-            #for multi UUT
-            for UUT_IP in UUT_IPs:
-                #while True:
-                #print(UUT_IP)
-                try:
-                    UUT_Return = str(get_control(UUT_IP,UUT_cmd)) #ip, cmd
-                    if UUT_Return=="U:OK, just do it.":
-                        print("C:OK")
-                    else:
-                        print("C:UUT_Return:",UUT_Return)
-                except:
-                    print(UUT_IP,"connection fail.")
-    """
-
-
-
-
-
+def scan_uut():
+    pass
 if __name__ == "__main__":  # Start from here
-    os.system('mode con: cols=80 lines=80')
-    UUT_IPs=["192.168.0.178","192.168.0.22"]
+    os.system('mode con: cols=80 lines=30')
+    UUT_IPs=["192.168.1.178","192.168.1.22","192.168.1.31","192.168.0.34"]
     while True:
         main_menu()
-
-"""
-Know issue buffer not ready ?p?G?ǤӦhdata?|lose data
-"""
